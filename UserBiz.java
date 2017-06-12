5f8c11e6923845ea7df87ea7896b0cd263b94799
yingluoshuangai <yezixingkong@gmail.com>
2017-06-12 22:38:38 +0800

    UserBiz.java-使用HQL语句

--- UserBiz.java
+++ UserBiz.java
@@ -1,5 +1,8 @@
 package biz;
 
+import java.util.List;
+import java.util.Map;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Transaction;
 
@@ -70,4 +73,62 @@ public class UserBiz {
 		return user;
 	}
 	
+	/*
+	 * 动态查询User表
+	 * 条件1：username列为admin
+	 * 条件2：telephone在15000000000和1800000000之间*/
+	public List<User> findUsers(Map<String,Object> user){
+		Transaction tx = null;
+		List<User> users = null;
+		
+		StringBuilder hql = new StringBuilder("from User u where 1=1");
+		if(user != null){
+			if(user.get("username") != null){
+				hql.append(" and u.username = :username ");
+			}
+			if(user.get("begin") != null){
+				hql.append(" and u.telephone > :begin ");
+			}
+			if(user.get("end") != null){
+				hql.append(" and u.telephone < :end ");
+			}
+		}
+		try{
+			tx = HibernateSessionFactory.getSession().beginTransaction();
+			users = dao.findUsers(hql.toString(), user);
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+			if(tx != null){
+				tx.rollback();
+			}
+		}
+		return users;
+				
+	}
+	
+	/*分页查询User表*/
+	public List<User> findUsers(int page,int size){
+		Transaction tx = null;
+		List<User> users = null;
+		try{
+			tx = HibernateSessionFactory.getSession().beginTransaction();
+			users = dao.findUsers(page,size);
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+			if(tx != null){
+				tx.rollback();
+			}
+		}
+		return users;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
+
